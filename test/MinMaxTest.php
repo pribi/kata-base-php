a64ae13a72ac9d68986beff6e40018bdcdb464f6
pribi <amokacsi2@gmail.com>
2014-09-14 20:39:25 +0200

    MinMax data providers

--- test/MinMaxTest.php
+++ test/MinMaxTest.php
@@ -12,41 +12,89 @@ class MinMaxTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * Tests min()
+     * @dataProvider minDataProvider
      */
-    public function testMin()
+    public function testMin($result, $numbers)
     {
-        $minMax = new MinMax(array(6, 9, 15, -2, 92, 11));
+        $minMax = new MinMax($numbers);
 
-        $this->assertEquals(-2, $minMax->min());
+        $this->assertEquals($result, $minMax->min());
     }
 
     /**
      * Tests max()
+     * @dataProvider maxDataProvider
      */
-    public function testMax()
+    public function testMax($result, $numbers)
     {
-        $minMax = new MinMax(array(6, 9, 15, -2, 92, 11));
+        $minMax = new MinMax($numbers);
 
-        $this->assertEquals(92, $minMax->max());
+        $this->assertEquals($result, $minMax->max());
     }
 
     /**
      * Tests count()
+     * @dataProvider countDataProvider
      */
-    public function testCount()
+    public function testCount($result, $numbers)
     {
-        $minMax = new MinMax(array(6, 9, 15, -2, 92, 11));
+        $minMax = new MinMax($numbers);
 
-        $this->assertEquals(6, $minMax->count());
+        $this->assertEquals($result, $minMax->count());
     }
 
     /**
      * Tests average
+     * @dataProvider averageDataProvider
      */
-    public function testAverage()
+    public function testAverage($result, $numbers)
     {
-        $minMax = new MinMax(array(6, 9, 15, -2, 92, 11));
+        $minMax = new MinMax($numbers);
 
-        $this->assertEquals(21.833333, $minMax->average());
+        $this->assertEquals($result, $minMax->average());
+    }
+
+    /**
+     * Min data provider
+     * @return array
+     */
+    public function minDataProvider()
+    {
+        return array(
+            array(-2, array(6, 9, 15, -2, 92, 11))
+        );
+    }
+
+    /**
+     * Max data provider
+     * @return array
+     */
+    public function maxDataProvider()
+    {
+        return array(
+            array(92, array(6, 9, 15, -2, 92, 11))
+        );
+    }
+
+    /**
+     * Count data provider
+     * @return array
+     */
+    public function countDataProvider()
+    {
+        return array(
+            array(6, array(6, 9, 15, -2, 92, 11))
+        );
+    }
+
+    /**
+     * Max data provider
+     * @return array
+     */
+    public function averageDataProvider()
+    {
+        return array(
+            array(21.833333, array(6, 9, 15, -2, 92, 11))
+        );
     }
 }
