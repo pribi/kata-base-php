bd26f4aa36b2b29d8d832fae06a3684af6344604
pribi <amokacsi2@gmail.com>
2014-10-16 00:37:28 +0200

    VelocityChecker useCaptcha test cases 3, 500, 1000

--- src/AntiBruteForce/VelocityChecker.php
+++ src/AntiBruteForce/VelocityChecker.php
@@ -9,13 +9,50 @@ namespace Kata\AntiBruteForce;
  * @author Janos Pribelszki
  */
 class VelocityChecker {
+
+    /**
+     * Maximum number of failed login attempts from one IP - after captcha is being displayed
+     */
+    const MAX_LOGIN_IP = 3;
+
+    /**
+     * Maximum number of failed login attempts from one IP range - after captcha is being displayed
+     */
+    const MAX_LOGIN_RANGE = 500;
+
+    /**
+     * Maximum number of failed login attempts from one IP country - after captcha is being displayed
+     */
+    const MAX_LOGIN_COUNTRY = 1000;
+
     /**
      * Returns true if we use captcha; false otherwise
      *
+     * @param array $loginAttempt Current login attempt
+     * @param Counter $counter    Failed login attempts counter
+     *
      * @return bool
      */
-    public function useCaptcha()
+    public function useCaptcha($loginAttempt, Counter $counter)
     {
+        // IP
+        if ($counter->getFailedLoginCountIp($loginAttempt['ip']) >= self::MAX_LOGIN_IP) {
+            return true;
+        }
+
+        // IP range
+        $ipArr = explode('.', $loginAttempt['ip']);
+        $ipArr[3] = 'x';
+        $ipX = implode('.', $ipArr);
+        if ($counter->getFailedLoginCountIpRange($ipX) >= self::MAX_LOGIN_RANGE) {
+            return true;
+        }
+
+        // IP country
+        if ($counter->getFailedLoginCountIpCountry($loginAttempt['ip_country']) >= self::MAX_LOGIN_COUNTRY) {
+            return true;
+        }
+
         return false;
     }
 }
--- test/AntiBruteForce/VelocityCheckerTest.php
+++ test/AntiBruteForce/VelocityCheckerTest.php
@@ -38,6 +38,54 @@ class VelocityCheckerTest extends \PHPUnit_Framework_TestCase
      */
     public function useCaptchaDataProvider()
     {
+        // 2 requests from same IP
+        $requests1 = array(
+            array('ip' => '192.168.0.1', 'ip_country' => 'US', 'username' => 'bill'),
+            array('ip' => '192.168.0.2', 'ip_country' => 'US', 'username' => 'bill'),
+        );
+
+        // 3 requests from same IP
+        $requests2 = array(
+            array('ip' => '192.168.0.1', 'ip_country' => 'US', 'username' => 'bill'),
+            array('ip' => '192.168.0.1', 'ip_country' => 'US', 'username' => 'bill'),
+            array('ip' => '192.168.0.1', 'ip_country' => 'US', 'username' => 'bill'),
+        );
+
+        // 499 requests from same IP range
+        $requests3 = array();
+        for ($i = 0; $i < 499; $i++) {
+            $requests3[] = array('ip' => '192.168.0.'.intval($i / 2), 'ip_country' => 'US', 'username' => 'bill');
+        }
+
+        // 500 requests from same IP range
+        $requests4 = array();
+        for ($i = 0; $i < 500; $i++) {
+            $requests4[] = array('ip' => '192.168.0.'.intval($i / 2), 'ip_country' => 'US', 'username' => 'bill');
+        }
+
+        // 999 requests from same IP country
+        $requests5 = array();
+        for ($i = 0; $i < 499; $i++) {
+            $requests5[] = array('ip' => '192.168.0.'.intval($i / 2), 'ip_country' => 'US', 'username' => 'bill');
+        }
+        for ($i = 0; $i < 499; $i++) {
+            $requests5[] = array('ip' => '192.168.1.'.intval($i / 2), 'ip_country' => 'US', 'username' => 'bill');
+        }
+        for ($i = 0; $i < 1; $i++) {
+            $requests5[] = array('ip' => '192.168.2.'.intval($i / 2), 'ip_country' => 'US', 'username' => 'bill');
+        }
+
+        // 1000 requests from same IP range
+        for ($i = 0; $i < 499; $i++) {
+            $requests6[] = array('ip' => '192.168.0.'.intval($i / 2), 'ip_country' => 'US', 'username' => 'bill');
+        }
+        for ($i = 0; $i < 499; $i++) {
+            $requests6[] = array('ip' => '192.168.1.'.intval($i / 2), 'ip_country' => 'US', 'username' => 'bill');
+        }
+        for ($i = 0; $i < 2; $i++) {
+            $requests6[] = array('ip' => '192.168.2.'.intval($i / 2), 'ip_country' => 'US', 'username' => 'bill');
+        }
+
         return array(
             array(
                 false,
@@ -46,11 +94,53 @@ class VelocityCheckerTest extends \PHPUnit_Framework_TestCase
                     'ip_country' => 'US',
                     'username' => 'bill',
                 ),
+                $requests1
+            ),
+            array(
+                true,
                 array(
-                    array('ip' => '192.168.0.1', 'ip_country' => 'US', 'username' => 'bill'),
-                    array('ip' => '192.168.0.2', 'ip_country' => 'US', 'username' => 'bill'),
-                )
-            )
+                    'ip' => '192.168.0.1',
+                    'ip_country' => 'US',
+                    'username' => 'bill',
+                ),
+                $requests2
+            ),
+            array(
+                false,
+                array(
+                    'ip' => '192.168.0.1',
+                    'ip_country' => 'US',
+                    'username' => 'bill',
+                ),
+                $requests3
+            ),
+            array(
+                true,
+                array(
+                    'ip' => '192.168.0.1',
+                    'ip_country' => 'US',
+                    'username' => 'bill',
+                ),
+                $requests4
+            ),
+            array(
+                false,
+                array(
+                    'ip' => '192.168.0.1',
+                    'ip_country' => 'US',
+                    'username' => 'bill',
+                ),
+                $requests5
+            ),
+            array(
+                true,
+                array(
+                    'ip' => '192.168.0.1',
+                    'ip_country' => 'US',
+                    'username' => 'bill',
+                ),
+                $requests6
+            ),
         );
     }
 }
