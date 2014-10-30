f67937a3b7b377bfd77eda5297777dc049af0b82
pribi <amokacsi2@gmail.com>
2014-10-30 18:39:51 +0100

    LegacyProd 2 test green

--- test/LegacyProduct/ProductDAOTest.php
+++ test/LegacyProduct/ProductDAOTest.php
@@ -21,29 +21,91 @@ class ProductDAOTest extends PHPUnit_Framework_TestCase
 
 
     /**
-     * @TODO more test cases with dataProvider!
-     * @TODO empty result test case: return new NullProduct;
+     * @param mixed $result
+     * @param string $ean
+     * @param array $products
+     *
+     * @dataProvider getByEanDataProvider
      */
-    public function testGetByEan()
+    public function testGetByEan($result, $ean, $products)
     {
-        $stmt = $this->pdo->prepare(
-            "INSERT INTO product (id, ean, name) VALUES (1, '', '');"
-        );
-        $stmt->execute();
+        if (!empty($products)) {
+            foreach ($products as $product) {
+                $stmt = $this->pdo->prepare(
+                    "INSERT INTO product (id, ean, name) VALUES (".$product['id'].", '".$product['ean']."', '".$product['name']."');"
+                );
+                $stmt->execute();
+            }
+        }
+
 
         $productDao = new ProductDao($this->pdo);
-        $product = $productDao->getByEan('');
-
-        $this->assertEquals(1, $product->id);
-        $this->assertEquals('', $product->ean);
-        $this->assertEquals('', $product->name);
+        $this->assertEquals($result, $productDao->getByEan($ean));
     }
 
     /**
-     * @TODO similar to testGetByEan
+     * @param mixed $result
+     * @param int $id
+     * @param array $products
+     *
+     * @dataProvider getByIdDataProvider
      */
-    public function testGetById()
+    public function testGetById($result, $id, $products)
     {
+        if (!empty($products)) {
+            foreach ($products as $product) {
+                $stmt = $this->pdo->prepare(
+                    "INSERT INTO product (id, ean, name) VALUES (".$product['id'].", '".$product['ean']."', '".$product['name']."');"
+                );
+                $stmt->execute();
+            }
+        }
+
+
+        $productDao = new ProductDao($this->pdo);
+        $this->assertEquals($result, $productDao->getById($id));
+    }
+
+    public function getByEanDataProvider()
+    {
+        $product1 = new Product();
+        $product1->id = 1;
+        $product1->ean = '';
+        $product1->name = '';
+
+        $product2 = new Product();
+        $product2->id = 2;
+        $product2->ean = '2';
+        $product2->name = '2';
+
+        return array(
+            array(new NullProduct(), '', array()),
+            array($product1, '', array(array('id' => 1, 'ean' => '', 'name' => ''))),
+            array($product2, '2', array(array('id' => 2, 'ean' => '2', 'name' => '2'))),
+            array($product1, '', array(array('id' => 1, 'ean' => '', 'name' => ''), array('id' => 2, 'ean' => '2', 'name' => '2'))),
+            array($product2, '2', array(array('id' => 1, 'ean' => '', 'name' => ''), array('id' => 2, 'ean' => '2', 'name' => '2'))),
+        );
+    }
+
+    public function getByIdDataProvider()
+    {
+        $product1 = new Product();
+        $product1->id = 1;
+        $product1->ean = '';
+        $product1->name = '';
+
+        $product2 = new Product();
+        $product2->id = 2;
+        $product2->ean = '2';
+        $product2->name = '2';
+
+        return array(
+            array(new NullProduct(), '', array()),
+            array($product1, 1, array(array('id' => 1, 'ean' => '', 'name' => ''))),
+            array($product2, 2, array(array('id' => 2, 'ean' => '2', 'name' => '2'))),
+            array($product1, 1, array(array('id' => 1, 'ean' => '', 'name' => ''), array('id' => 2, 'ean' => '2', 'name' => '2'))),
+            array($product2, 2, array(array('id' => 1, 'ean' => '', 'name' => ''), array('id' => 2, 'ean' => '2', 'name' => '2'))),
+        );
     }
 
     public function testCreate()
