be377a9721a56da5e5f1522391c4b2c78e70862d
pribi <amokacsi2@gmail.com>
2014-10-30 18:20:39 +0100

    LegacyProduct test #1

--- test/LegacyProduct/ProductDAOTest.php
+++ test/LegacyProduct/ProductDAOTest.php
@@ -1,11 +1,47 @@
 <?php
 
+define('TEST_DATABASE_FILE', './test.db');
+require_once("../../src/LegacyProduct/ProductDao.php");
+require_once("../../src/LegacyProduct/Product.php");
 class ProductDAOTest extends PHPUnit_Framework_TestCase
 {
-    public function testGetByEan()
+    private $pdo;
+
+    public function setup()
     {
+        $dsn = sprintf("sqlite:%s", TEST_DATABASE_FILE);
+        $this->pdo = new PDO($dsn);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $this->pdo->prepare(
+            "CREATE TABLE product ( id INTEGER PRIMARY KEY, ean varchar(64) default '', name text default '' );"
+        );
+        $stmt->execute();
     }
 
+
+    /**
+     * @TODO more test cases with dataProvider!
+     * @TODO empty result test case: return new NullProduct;
+     */
+    public function testGetByEan()
+    {
+        $stmt = $this->pdo->prepare(
+            "INSERT INTO product (id, ean, name) VALUES (1, '', '');"
+        );
+        $stmt->execute();
+
+        $productDao = new ProductDao($this->pdo);
+        $product = $productDao->getByEan('');
+
+        $this->assertEquals(1, $product->id);
+        $this->assertEquals('', $product->ean);
+        $this->assertEquals('', $product->name);
+    }
+
+    /**
+     * @TODO similar to testGetByEan
+     */
     public function testGetById()
     {
     }
@@ -20,5 +56,10 @@ class ProductDAOTest extends PHPUnit_Framework_TestCase
 
     public function testDelete()
     {
+    }
+
+    public function tearDown()
+    {
+        unlink(TEST_DATABASE_FILE);
     }
 }
